8d9d493c311caa9234f602d5860224529f804448
AdLap <a.lapacz@global4net.com>
2023-04-06 18:36:50 +0200

    BTN-8

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,10 +7,10 @@
 
       <VRow class="d-flex align-center justify-center">
         <VCol cols="auto">
-          <MyButton color="primary" size="small" text="Przelicz talie" @click="deckCount(randomCards, baseCards)">
+          <MyButton color="primary" size="small" text="Przelicz talie" @click="deckCount(randomCards, baseDeck)">
             <template #append>dodatkowa treść</template>
           </MyButton>
-          <MyButton color="secondary" size="small" text="Wylosuj karty" @click="shuffle(baseCards)" />
+          <MyButton color="secondary" size="small" text="Wylosuj karty" @click="shuffleCards(baseDeck)" />
           <DeckInfo :deck=deck />
         </VCol>
       </VRow>
@@ -20,52 +20,28 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { randomNumber, baseCards } from '@/helpers/helpers.js'
-import type { Cards } from '@/type/types'
+import { baseDeck, sortCards, shuffle } from '@/helpers/helpers.js'
 import MyButton from '@/components/MyButton.vue'
 import DeckInfo from '@/components/DeckInfo.vue'
 
 const deck = ref<number>(0)
 const randomCards = ref<string[]>([])
 
-/**
- * 
- * @param baseCards deck schema
- */
-const shuffle = (baseCards: string[]): void => {
-  const cards: string[] = []
-  const totalCards: number = randomNumber(50, 200)
-
-  for (let i = 0; i < totalCards; i++) {
-    let index = randomNumber(0, baseCards.length - 1)
-    cards.push(
-      baseCards[index]
-    )
-  }
-
-  randomCards.value = [...cards]
+const shuffleCards = (baseDeck: string[]): void => {
+  const newCards = shuffle(baseDeck)
+  randomCards.value = [...newCards]
 }
 
 /**
- * 
  * @param cards cards to sort
- * @param baseCards deck schema
+ * @param baseDeck deck schema
  */
-const deckCount = (cards: string[], baseCards: string[]): void => {
+const deckCount = (cards: string[], baseDeck: string[]): void => {
   let deckNumber = 0
-  const sortedCards: Cards = {}
-
-  // card sorting
-  cards.forEach((card: string): void => {
-    if (!Object.keys(sortedCards).includes(card)) {
-      sortedCards[card] = 1
-    } else {
-      sortedCards[card]++
-    }
-  })
+  const sortedCards = sortCards(cards)
 
   // checking that at least one collection is complete
-  if (Object.keys(cards).length < baseCards.length) {
+  if (cards === undefined || Object.keys(cards).length < baseDeck.length) {
     deck.value = 0
     return
   }
--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,9 +1,46 @@
-export const baseCards: string[] = [
-  'WPIK', 'WKIE', 'WTRE', 'WKAR',
-  'DPIK', 'DKIE', 'DTRE', 'DKAR',
-  'KPIK', 'KKIE', 'KTRE', 'KKAR',
-  'APIK', 'AKIE', 'ATRE', 'AKAR'
-]
+import { Cards } from "@/type/types"
+
+export const baseDeck: string[] = [
+  "WPIK", "WKIE", "WTRE", "WKAR",
+  "DPIK", "DKIE", "DTRE", "DKAR",
+  "KPIK", "KKIE", "KTRE", "KKAR",
+  "APIK", "AKIE", "ATRE", "AKAR",
+];
 
 export const randomNumber = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min + 1) + min)
+
+export const sortCards = (cards: string[]): Cards => {
+  const sortedCards: Cards = {}
+
+  if (!cards) {
+    return {}
+  }
+
+  cards.forEach((card: string): void => {
+    if (!Object.keys(sortedCards).includes(card)) {
+      sortedCards[card] = 1
+    } else {
+      sortedCards[card]++
+    }
+  })
+
+  return sortedCards
+}
+
+/**
+ * @param baseDeck deck schema
+ */
+export const shuffle = (baseDeck: string[]): string[] => {
+  const cards: string[] = []
+  const totalCards: number = randomNumber(50, 200)
+
+  for (let i = 0; i < totalCards; i++) {
+    const index = randomNumber(0, baseDeck.length - 1)
+    cards.push(
+      baseDeck[index]
+    )
+  }
+
+  return cards
+}
